a017ef9afa3af8753d50214f72e9e012c2f79d4a
Steven Thomas Smith <s.t.smith@ieee.org>
2017-07-19 06:02:21 -0400

    Turn off large debug CSS output files

--- adblock2privoxy/src/ElementBlocker.hs
+++ adblock2privoxy/src/ElementBlocker.hs
@@ -31,35 +31,57 @@ elemBlock path info = writeElemBlock . elemBlockData
            cont <- getDirectoryContents path
            _ <- sequence $ removeOld <$> cont
            createDirectoryIfMissing True debugPath
-           writeBlockTree path debugPath rulesTree
-           writePatterns filteredInfo (path </> "ab2p.common.css") (debugPath </> "ab2p.common.css") flatPatterns
+           -- writeBlockTree path debugPath rulesTree
+           writeBlockTree path rulesTree
+           writePatterns_with_debug filteredInfo (path </> "ab2p.common.css") (debugPath </> "ab2p.common.css") flatPatterns
     removeOld entry' =
         let entry = path </> entry'
         in do
            isDir <- doesDirectoryExist entry
            if isDir then when (head entry' /= '.') $ removeDirectoryRecursive entry
                     else when (takeExtension entry == ".css") $ removeFile entry
-    writeBlockTree :: String -> String -> BlockedRulesTree -> IO ()
-    writeBlockTree normalNodePath debugNodePath (Node name patterns children) =
+    -- writeBlockTree :: String -> String -> BlockedRulesTree -> IO ()
+    -- writeBlockTree normalNodePath debugNodePath (Node name patterns children) =
+    writeBlockTree :: String -> BlockedRulesTree -> IO ()
+    writeBlockTree normalNodePath (Node name patterns children) =
         do
             createDirectoryIfMissing True normalPath
-            createDirectoryIfMissing True debugPath
-            _ <- sequence (writeBlockTree normalPath debugPath <$> children)
-            writePatterns ["See ab2p.common.css for sources info"] normalFilename debugFilename patterns
+            -- createDirectoryIfMissing True debugPath
+            -- _ <- sequence (writeBlockTree normalPath debugPath <$> children)
+            -- writePatterns ["See ab2p.common.css for sources info"] normalFilename debugFilename patterns
+            _ <- sequence (writeBlockTree normalPath <$> children)
+            writePatterns ["See ab2p.common.css for sources info"] normalFilename patterns
         where
             normalPath
                 | null name = normalNodePath
                 | otherwise = normalNodePath </> name
-            debugPath
-                | null name = debugNodePath
-                | otherwise = debugNodePath </> name
+            -- debugPath
+            --     | null name = debugNodePath
+            --     | otherwise = debugNodePath </> name
             normalFilename = normalPath </> "ab2p.css"
-            debugFilename = debugPath </> "ab2p.css"
-    writePatterns :: [String] -> String -> String -> [Pattern] -> IO ()
-    writePatterns _ _ _ [] = return ()
-    writePatterns info' normalFilename debugFilename patterns =
+            -- debugFilename = debugPath </> "ab2p.css"
+    writePatterns_with_debug :: [String] -> String -> String -> [Pattern] -> IO ()
+    writePatterns_with_debug _ _ _ [] = return ()
+    writePatterns_with_debug info' normalFilename debugFilename patterns =
          do
             writeCssFile debugFilename $ intercalate "\n" $ (++ Templates.blockCss) <$> patterns
+            writeCssFile normalFilename $ intercalate "\n" ((++ Templates.blockCss) . intercalate "," <$>
+                                                                            splitEvery 4000 patterns)
+         where
+         splitEvery n = takeWhile (not . null) . unfoldr (Just . splitAt n)
+         writeCssFile filename content =
+                do outFile <- openFile filename WriteMode
+                   hSetEncoding outFile utf8
+                   hPutStrLn outFile "/*"
+                   _ <- mapM (hPutStrLn outFile) info'
+                   hPutStrLn outFile "*/"
+                   hPutStrLn outFile content
+                   hClose outFile
+    writePatterns :: [String] -> String -> [Pattern] -> IO ()
+    writePatterns _ _ [] = return ()
+    writePatterns info' normalFilename patterns =
+         do
+            -- writeCssFile debugFilename $ intercalate "\n" $ (++ Templates.blockCss) <$> patterns
             writeCssFile normalFilename $ intercalate "\n" ((++ Templates.blockCss) . intercalate "," <$>
                                                                             splitEvery 4000 patterns)
          where
